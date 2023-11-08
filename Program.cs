38c1f5e9aefd2247668c6c32fa4edb6308fb3f45
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-11-09 00:03:06 +0530

    Switch Case Done

--- Program.cs
+++ Program.cs
@@ -93,7 +93,40 @@ namespace HelloWorld
                 Console.WriteLine("Your grade is F");
             }
 
-            //Console.ReadLine();
+
+            // Switch Case
+
+            int num;
+            Console.Write("Enter a number between 1-7 : ");
+            num = Int16.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1: 
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+
+            }
+
+            Console.ReadLine();
 
 
 
